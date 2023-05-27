93e37ac1e207283cdfc0388fdb3adb848de14943
Vadym Surovytskyi <vsurovytskyi@gmail.com>
2023-05-27 11:25:16 +0300

    2005

--- vadim/access/values.js
+++ vadim/access/values.js
@@ -3,7 +3,7 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
-var birthdate = "19.02.2004"; // Дата рождения
+var birthdate = "20.03.2005"; // Дата рождения
 var fullname = "Колос Вадим Артемович" // ФИО
 var name = "Вадим" // Имя
 
@@ -20,5 +20,5 @@ var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_valid_until = "01.01.2024" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
