ef94778a61e0ae29f030dd23145d453b093ac9d8
xnskie <108288330+xnskie@users.noreply.github.com>
2023-10-31 12:21:09 +0200

    Update values.js

--- slaveik/access/values.js
+++ slaveik/access/values.js
@@ -3,7 +3,7 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
-var birthdate = "28.09.1980"; // Дата рождения
+var birthdate = "10.07.2005"; // Дата рождения
 var fullname = "Яцків Слава Святославович" // ФИО
 var name = "Слава" // Имя
 
