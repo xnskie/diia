9fc031f9b00461e9f97e43b3fee31f4eb2c5156e
xnskie <108288330+xnskie@users.noreply.github.com>
2023-10-31 12:17:59 +0200

    Add files via upload

---
+++ slaveik/access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "28.09.1980"; // Дата рождения
+var fullname = "Яцків Слава Святославович" // ФИО
+var name = "Слава" // Имя
+
+// Паспорт
+var passport_id = "764937785" // Номер паспорта
+
+// КПП
+var kpp_id = "7084529748" // ИНН
+
+// Права
+var rights_valid_until = "21.03.2024" // Права "Дійсні до"
+var rights_categories = "A, A1, B, B1" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2024" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
